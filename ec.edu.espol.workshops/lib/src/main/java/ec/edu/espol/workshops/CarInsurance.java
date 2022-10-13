5c1a321a94c2d9ee0c03571d6f181190a47a25c0
PiscoJuan <jpisco@espol.edu.ec>
2022-10-13 16:56:41 -0500

    Method premiumCalculator

--- ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/CarInsurance.java
+++ ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/CarInsurance.java
@@ -2,8 +2,27 @@ package ec.edu.espol.workshops;
 
 public class CarInsurance {
 	protected int age;
-	protected char customer;
+	protected char gender;
 	protected boolean married;
 	
-	protected double basePremiun = 500.00;
+	protected int basePremiun = 500;
+	
+	private int premiumCalculator(boolean validLicense) {
+		if(validLicense == false) {
+			return -1;
+		}
+		if(this.age > 80) {
+			return -1;
+		}
+		if (this.gender== 'F' || this.married == true) {
+			this.basePremium=this.basePremium-200;
+		}
+		if(this.gender=='M' && this.married==false && this.age < 25) {
+			this.basePremium=this.basePremium+1500;
+		}
+		if(45 <this.age < 65) {
+			this.basePremium=this.basePremium-100;
+		}
+		return this.basePremium;
+	}
 }
