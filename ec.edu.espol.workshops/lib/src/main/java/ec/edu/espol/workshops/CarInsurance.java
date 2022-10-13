1000914a299016436acd23c0e4ba7c07eb719f0f
lurapozo <lurapozo@lrp-lp>
2022-10-13 16:40:05 -0500

    class attributes of CarInsurance

--- ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/CarInsurance.java
+++ ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/CarInsurance.java
@@ -1,5 +1,9 @@
 package ec.edu.espol.workshops;
 
 public class CarInsurance {
-
+	protected int age;
+	protected char customer;
+	protected boolean married;
+	
+	protected double basePremiun = 500.00;
 }
