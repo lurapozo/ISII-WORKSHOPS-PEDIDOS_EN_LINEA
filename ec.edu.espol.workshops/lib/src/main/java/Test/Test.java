b5537ec5fb18ebe53b40cb31363d45d4656fe989
PiscoJuan <jpisco@espol.edu.ec>
2022-11-10 22:52:28 -0500

    "Testing the outputs"

--- ec.edu.espol.workshops/lib/src/main/java/Test/Test.java
+++ ec.edu.espol.workshops/lib/src/main/java/Test/Test.java
@@ -6,11 +6,54 @@ public class Test {
 	public static void main (String [ ] args) {
 		System.out.println ("Empezamos la ejecución del programa");
 		CarInsurance Prueba = new CarInsurance();
-		Prueba.setAge(55);
+		boolean license;
+		//TC1
+		Prueba.setBasePremium(500);
+		Prueba.setAge(20);
 		Prueba.setGender('M');
 		Prueba.setMarried(false);
-		System.out.print(Prueba.getBasePremium());
+		license=true;
+		System.out.println("Prueba TC1 esperado: 2000   obtenido: " + Prueba.premiumCalculator(license));
 		
+		//TC2
+		Prueba.setBasePremium(500);
+		Prueba.setAge(50);
+		Prueba.setGender('F');
+		Prueba.setMarried(true);
+		license=true;
+		System.out.println("Prueba TC2 esperado: 200   obtenido: " + Prueba.premiumCalculator(license));
+		
+		//TC3
+		Prueba.setBasePremium(500);
+		Prueba.setAge(50);
+		Prueba.setGender('F');
+		Prueba.setMarried(true);
+		license=false;
+		System.out.println("Prueba TC3 esperado: -1   obtenido: " + Prueba.premiumCalculator(license));
+
+		//TC4
+		Prueba.setBasePremium(500);
+		Prueba.setAge(70);
+		Prueba.setGender('M');
+		Prueba.setMarried(false);
+		license=true;
+		System.out.println("Prueba TC4 esperado: 500   obtenido: " + Prueba.premiumCalculator(license));
+
+		//TC5
+		Prueba.setBasePremium(500);
+		Prueba.setAge(16);
+		Prueba.setGender('M');
+		Prueba.setMarried(false);
+		license=true;
+		System.out.println("Prueba TC5 esperado: -1   obtenido: " + Prueba.premiumCalculator(license));
+		
+		//TC6
+		Prueba.setBasePremium(500);
+		Prueba.setAge(100);
+		Prueba.setGender('M');
+		Prueba.setMarried(false);
+		license=true;
+		System.out.println("Prueba TC6 esperado: -1   obtenido: " + Prueba.premiumCalculator(license));
 	}
 	
 }
---
+++ ec.edu.espol.workshops/lib/src/test/java/ec/edu/espol/workshops/CarInsuranceTest.java
@@ -0,0 +1,29 @@
+package ec.edu.espol.workshops;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+//import org.junit.Test;
+
+//import static org.junit.jupiter.api.Assertions.fail;
+
+//import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+
+//import org.junit.runner.RunWith;
+
+
+public class CarInsuranceTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+
+}
+
